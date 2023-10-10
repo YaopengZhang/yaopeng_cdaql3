a5841f2575312365d8f6ad07a942c9a34bdd327c
cdaq <cdaq@jlab.org>
2023-10-10 12:47:59 -0400

    10-10-1

--- Temperature_plots/manual_analyze/Get_data.sh
+++ Temperature_plots/manual_analyze/Get_data.sh
@@ -3,7 +3,7 @@
 # myData -b "2023-10-01 $time_start" -e "2023-10-01 $time_end" hchv30:00:000:VMon  > test.txt
 # myData -b "2023-10-01 $time_start" -e "2023-10-01 $time_end" hcnps_intlk_cz_t_back_1  > ./data/chart-1.txt
 
-previous_date=$(cat "/home/cdaq/yaopeng/Temperature_plots/date.txt")
+previous_date=$(cat "/home/cdaq/yaopeng/Temperature_plots/manual_analyze/date.txt")
 IFS="-" read -ra date_parts <<< "$previous_date"
 year="${date_parts[0]}"
 month="${date_parts[1]}"
@@ -13,22 +13,22 @@ echo "Year: $year"
 echo "Month: $month"
 echo "Day: $day"
 
-time_start="00:00:00"
-time_end="23:59:59"
+time_start="08:00:00"
+time_end="18:00:00"
 
-/home/cdaq/bin/myData -b "$previous_date $time_start" -e "$previous_date $time_end" hcnps_intlk_chiller_CZ_read_chiller_temp > /home/cdaq/yaopeng/Temperature_plots/data/chart-CZ.txt
-/home/cdaq/bin/myData -b "$previous_date $time_start" -e "$previous_date $time_end" hcnps_intlk_chiller_EZ_read_chiller_temp > /home/cdaq/yaopeng/Temperature_plots/data/chart-EZ.txt
-/home/cdaq/bin/myData -b "$previous_date $time_start" -e "$previous_date $time_end" hchv30:00:000:VMon > /home/cdaq/yaopeng/Temperature_plots/data/chart-HV.txt
-/home/cdaq/bin/myData -b "$previous_date $time_start" -e "$previous_date $time_end" ibcm1 > /home/cdaq/yaopeng/Temperature_plots/data/chart-Beam.txt
+/home/cdaq/bin/myData -b "$previous_date $time_start" -e "$previous_date $time_end" hcnps_intlk_chiller_CZ_read_chiller_temp > /home/cdaq/yaopeng/Temperature_plots/manual_analyze/data/chart-CZ.txt
+/home/cdaq/bin/myData -b "$previous_date $time_start" -e "$previous_date $time_end" hcnps_intlk_chiller_EZ_read_chiller_temp > /home/cdaq/yaopeng/Temperature_plots/manual_analyze/data/chart-EZ.txt
+/home/cdaq/bin/myData -b "$previous_date $time_start" -e "$previous_date $time_end" hchv30:00:000:VMon > /home/cdaq/yaopeng/Temperature_plots/manual_analyze/data/chart-HV.txt
+/home/cdaq/bin/myData -b "$previous_date $time_start" -e "$previous_date $time_end" ibcm1 > /home/cdaq/yaopeng/Temperature_plots/manual_analyze/data/chart-Beam.txt
 
 for num in {1..56}
 do
     echo "---------------------------------------------------"
     echo "Getting data for hcnps_intlk_cz_t_back_${num}"
     var1="hcnps_intlk_cz_t_back_${num}"
-    /home/cdaq/bin/myData -b "$previous_date $time_start" -e "$previous_date $time_end" ${var1} > /home/cdaq/yaopeng/Temperature_plots/data/chart-${num}.txt
+    /home/cdaq/bin/myData -b "$previous_date $time_start" -e "$previous_date $time_end" ${var1} > /home/cdaq/yaopeng/Temperature_plots/manual_analyze/data/chart-${num}.txt
     new_num=$((num + 56))
     echo "Getting data for hcnps_intlk_cz_t_front_${num}"
     var2="hcnps_intlk_cz_t_front_${num}"
-    /home/cdaq/bin/myData -b "$previous_date $time_start" -e "$previous_date $time_end" ${var2} > /home/cdaq/yaopeng/Temperature_plots/data/chart-${new_num}.txt
+    /home/cdaq/bin/myData -b "$previous_date $time_start" -e "$previous_date $time_end" ${var2} > /home/cdaq/yaopeng/Temperature_plots/manual_analyze/data/chart-${new_num}.txt
 done
--- Temperature_plots/manual_analyze/make_daily_plots.csh
+++ Temperature_plots/manual_analyze/make_daily_plots.csh
@@ -2,26 +2,26 @@
 
 source /u/apps/root/6.08.00/setroot_CUE.csh
 
-root -l -b -q /home/cdaq/yaopeng/Temperature_plots/Analyze.C
+root -l -b -q /home/cdaq/yaopeng/Temperature_plots/manual_analyze/Analyze.C
 
 sleep 5
 
-set previous_date=`cat /home/cdaq/yaopeng/Temperature_plots/date.txt`
+set previous_date=`cat /home/cdaq/yaopeng/Temperature_plots/manual_analyze/date.txt`
 
 /site/ace/certified/apps/bin/logentry \
 -cert /home/cdaq/.elogcert \
 -t "NPS Temperature plots for ${previous_date}" \
 -e yaopeng \
 -l HCLOG \
--a "/home/cdaq/yaopeng/Temperature_plots/plots_hclog/temp_average_2D.pdf" \
--a "/home/cdaq/yaopeng/Temperature_plots/plots_hclog/temp_std_2D.pdf" \
--a "/home/cdaq/yaopeng/Temperature_plots/plots_hclog/temp_front_1D_array.pdf" \
--a "/home/cdaq/yaopeng/Temperature_plots/plots_hclog/temp_back_1D_array.pdf" \
--a "/home/cdaq/yaopeng/Temperature_plots/plots_hclog/temp_front_1D.pdf" \
--a "/home/cdaq/yaopeng/Temperature_plots/plots_hclog/temp_back_1D.pdf" \
--a "/home/cdaq/yaopeng/Temperature_plots/plots_hclog/temp_chiller_readout.pdf" \
--a "/home/cdaq/yaopeng/Temperature_plots/plots_hclog/Beam_status.pdf" \
--a "/home/cdaq/yaopeng/Temperature_plots/plots_hclog/HV_status.pdf" \
+-a "/home/cdaq/yaopeng/Temperature_plots/manual_analyze/plots_hclog/temp_average_2D.pdf" \
+-a "/home/cdaq/yaopeng/Temperature_plots/manual_analyze/plots_hclog/temp_std_2D.pdf" \
+-a "/home/cdaq/yaopeng/Temperature_plots/manual_analyze/plots_hclog/temp_front_1D_array.pdf" \
+-a "/home/cdaq/yaopeng/Temperature_plots/manual_analyze/plots_hclog/temp_back_1D_array.pdf" \
+-a "/home/cdaq/yaopeng/Temperature_plots/manual_analyze/plots_hclog/temp_front_1D.pdf" \
+-a "/home/cdaq/yaopeng/Temperature_plots/manual_analyze/plots_hclog/temp_back_1D.pdf" \
+-a "/home/cdaq/yaopeng/Temperature_plots/manual_analyze/plots_hclog/temp_chiller_readout.pdf" \
+-a "/home/cdaq/yaopeng/Temperature_plots/manual_analyze/plots_hclog/Beam_status.pdf" \
+-a "/home/cdaq/yaopeng/Temperature_plots/manual_analyze/plots_hclog/HV_status.pdf" \
 
-set current_date=`date +"%Y-%m-%d"`
-echo "$current_date" > /home/cdaq/yaopeng/Temperature_plots/date.txt
+# set current_date=`date +"%Y-%m-%d"`
+# echo "$current_date" > /home/cdaq/yaopeng/Temperature_plots/manual_analyze/date.txt
